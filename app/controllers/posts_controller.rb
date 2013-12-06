9fc945c320dd983ef4aa982d7bd4755fd6f53f8f
vivijie <j.uloveit@gmail.com>
2013-12-06 22:49:33 +0800

    Create all the typical actions for the Post resource (index, show, new, create, edit, update). Use model backed forms to create and edit post objects. Also extract the post form to a partial, to be used by both the new and edit templates. Add a validation and make sure the validation fires and you display the error on the template.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,48 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
 
   end
+
+  def edit
+    @post = Post.find(params[:id]) 
+  end
+
+  def update
+    # binding.pry
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+    # if @post.update(params[:post])
+      flash[:notice] = "The post updated."
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit!
+  end
+
 end
