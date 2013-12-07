0db935b57cbadc3e2110dd2903c2ef7bac13fe52
vivijie <j.uloveit@gmail.com>
2013-12-07 12:28:22 +0800

    Create a new category model-backed form, with validations. You'll need to add a route, create controller/actions.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,40 @@
+class CategoriesController < ApplicationController
+
+  def index
+  	@categories = Category.all
+  end
+
+  def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.new(category_params)
+  	if @category.save
+  	  flash[:notice] = "Category ceated!"
+  	  redirect_to categories_path
+  	else
+  	  render :new
+  	end
+  end
+
+  def edit
+  	@category = Category.find(params[:id])
+  end
+
+  def update
+  	@category = Category.find(params[:id])
+  	if @category.update(category_params)
+  	  flash[:notice] = "The category update!"
+  	  redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+
+  private
+  def category_params
+  	params.require(:category).permit!
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: [:destroy] do
-resources :comments, only: [:create, :show]
+    resources :comments, only: [:create, :show]
   end
+
+  resources :categories, except: [:destroy] 
 end
