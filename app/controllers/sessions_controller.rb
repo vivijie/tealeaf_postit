dc71e4dd879ba97172ad4ad009c34a74aedbb554
vivijie <j.uloveit@gmail.com>
2013-12-13 11:01:20 +0800

    remove session-controller's binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,10 @@ class SessionsController < ApplicationController
   def create
 
   	user = User.find_by(username: params[:username])
-      binding.pry
   	if user && user.authenticate(params[:password])
   	  session[:user_id] = user.id
   	  flash[:notice] = "You've logged in!"
   	  redirect_to root_path
-      binding.pry
   	else
   	  flash[:error] = "There's something wrong with your username or password."
   	  redirect_to register_path
@@ -23,7 +21,6 @@ class SessionsController < ApplicationController
   	session[:user_id] = nil
   	flash[:notice] = "You've logged out!"
   	redirect_to root_path
-    binding.pry
   end
 
 end
