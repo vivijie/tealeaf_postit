8ed2e881ccef528e425c61b6ed4d50a4e8f92b73
IObit_Mac_QA <QA@macs-Mac-mini.local>
2013-12-13 10:59:19 +0800

    validates :title, :url, :description, presence: true

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,5 @@ class Post < ActiveRecord::Base
   has_many		:categories, through: :post_categories
   has_many		:votes, as: :voteable
 
-  validates 	:title, presence: true
-  validates 	:url, presence: true
-  validates 	:description, presence: true
+  validates 	:title, :url, :description, presence: true
 end
