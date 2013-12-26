0cb0c8125dec6788db49e1c85ea526d88216a365
vivijie <j.uloveit@gmail.com>
2013-12-26 23:42:07 +0800

    add ajax to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,29 +4,34 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-  	@comment = Comment.new(params.require(:comment).permit(:body))
-  	@comment.post = @post
-  	@comment.creator = current_user
+    @comment = Comment.new(params.require(:comment).permit(:body))
+    @comment.post = @post
+    @comment.creator = current_user
 
-  	if @comment.save
-  	  flash[:notice] = "Your coment was added."
-  	  redirect_to post_path(@post)
-  	else
-  	  render 'posts/show'
-  	end
+    if @comment.save
+      flash[:notice] = "Your coment was added."
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
   end
 
   def vote
-    comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:id])
 
-    vote = Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
 
-    if vote.valid?
-      flash[:notice] = 'Your vote was counted.'
-    else
-      flash[:error] = 'Your have voted!'
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = 'Your vote was counted.'
+        else
+          flash[:error] = 'Your have voted!'
+        end
+        redirect_to :back
+      end
+      format.js
     end
-    redirect_to :back
   end
 
 
