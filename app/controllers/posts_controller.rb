45a92e8dd7a4921f3b73c010711a9f91dbf710b7
vivijie <j.uloveit@gmail.com>
2013-12-26 23:21:06 +0800

    add ajax to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,13 +35,19 @@ class PostsController < ApplicationController
   end
 
   def vote
-    vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
-    if vote.valid?
-      flash[:notice] = 'Your vote was counted.'
-    else
-      flash[:error] = 'Your have voted!'
+    @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = "Your vote was counted."
+        else
+          flash[:error] = 'You can only vote on a post once.'
+        end
+        redirect_to :back
+      end
+      format.js
     end
-    redirect_to :back
   end
 
 
