66aa81b385932e9ba3da429cea76ca0dd20d1aee
vivijie <j.uloveit@gmail.com>
2013-12-26 21:40:05 +0800

    add ajax to posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,16 +17,21 @@ class CommentsController < ApplicationController
   end
 
   def vote
-    comment = Comment.find(params[:id])
+    @comment = Comment.find(params[:id])
 
-    vote = Vote.create(voteable: comment, creator: current_user, vote: params[:vote])
+    @vote = Vote.create(voteable: @comment, creator: current_user, vote: params[:vote])
 
-    if vote.valid?
-      flash[:notice] = 'Your vote was counted.'
-    else
-      flash[:error] = 'Your have voted!'
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = 'Your vote was counted.'
+        else
+          flash[:error] = 'Your have voted!'
+        end
+        redirect_to :back
+      end
+      format.js
     end
-    redirect_to :back
   end
 
 
