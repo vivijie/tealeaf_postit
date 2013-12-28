cd7a19118768c1e0255dc0c24b5a7711ccb4580b
vivijie <j.uloveit@gmail.com>
2013-12-28 12:29:13 +0800

    deep slugs to fix same title point to same place

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,7 +26,30 @@ class Post < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.title.gsub(" ", "-").downcase 
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, "-"
+    str
   end
 
 end
